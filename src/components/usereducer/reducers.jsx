0d1b6c3046da5dd8a519cb143153660676545520
tahmid-emam <0.1tahmidemam@gmail.com>
2023-09-23 17:56:37 +0600

    added card

--- src/components/usereducer/reducers.jsx
+++ src/components/usereducer/reducers.jsx
@@ -40,13 +40,13 @@ const Reducers = () => {
     }
   return (
     <>
-    <div className='text-center'>
+    <div className='text-center mb-20'>
         <form onSubmit={handleSubmit}>
             <input className="input input-bordered input-primary w-full max-w-xs" type="text" placeholder='new data' value={newData} onChange={e => setNewData(e.target.value)} />
-            <button className='btn btn-primary ml-10' type="submit">Add</button>
+            <button className='btn btn-primary ml-10 px-7' type="submit">Add</button>
         </form>
     </div>
-      <div>
+      <div className='container mx-auto grid grid-cols-3 text-center'>
         {
             datas.map((data) => <SingleReducer key={data.id} data={data} />)
         }
--- src/components/usereducer/singleReducer.jsx
+++ src/components/usereducer/singleReducer.jsx
@@ -4,7 +4,17 @@ const SingleReducer = (props) => {
     const {id, name} = props.data
   return (
     <>
-      <h1 className='text-5xl'>{name}</h1>
+      <div className="mb-12">
+      <div className="card w-96 bg-primary text-primary-content">
+        <div className="card-body">
+          <h2 className="card-title">Card title!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      </div>
     </>
   )
 }
