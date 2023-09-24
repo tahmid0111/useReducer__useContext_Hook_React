f369ced8f234b068e7eb927731b31f6bae689f56
tahmid-emam <0.1tahmidemam@gmail.com>
2023-09-24 23:23:44 +0600

    some little change

--- src/components/usereducer/singleReducer.jsx
+++ src/components/usereducer/singleReducer.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
 const SingleReducer = (props) => {
-    const {id, name} = props.data
-    const generateRandomColor = () => {
-      const colors = [
-        'bg-red-500',
-        'bg-yellow-700',
-        'bg-green-900',
-        'bg-rose-950',
-        'bbg-rose-600',
-      ];
+    const {id, name, color} = props.data
+    // const generateRandomColor = () => {
+    //   const colors = [
+    //     'bg-red-500',
+    //     'bg-yellow-700',
+    //     'bg-green-900',
+    //     'bg-rose-950',
+    //     'bbg-rose-600',
+    //   ];
   
-      const randomIndex = Math.floor(Math.random() * colors.length);
-      return colors[randomIndex];
-    };
+    //   const randomIndex = Math.floor(Math.random() * colors.length);
+    //   return colors[randomIndex];
+    // };
 
   return (
     <>
-      <div className={`${generateRandomColor()} card w-96 text-primary-content mb-12`}>
+      <div className={`${color} card w-96 text-primary-content mb-12`}>
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
