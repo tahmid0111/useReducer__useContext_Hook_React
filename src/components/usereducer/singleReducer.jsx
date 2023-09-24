c4d8aa6059656888f9df192b6afe51ed9043ad60
tahmid-emam <0.1tahmidemam@gmail.com>
2023-09-24 23:25:52 +0600

    button color changed

--- src/components/usereducer/singleReducer.jsx
+++ src/components/usereducer/singleReducer.jsx
@@ -21,7 +21,7 @@ const SingleReducer = (props) => {
         <div className="card-body">
           <h2 className="card-title">{name}</h2>
           <div className="card-actions justify-end">
-            <button className="btn">Buy Now</button>
+            <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
